71898684f537fb715392320fa51e812e42f5143c
Sam Perozek <samperozek@gmail.com>
2022-06-07 19:04:25 -0500

    commit tree 5053ed48e4e258dffceb842ab5e533467b299893

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "SAM is the champion";
+newHeader.setAttribute("id", "victory");
